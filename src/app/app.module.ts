4d007d8763f74adb040043dbd38ed84acf653855
Merry <merrson1996@gmail.com>
2022-10-16 15:56:29 +0200

    Ostatnie zadanie part 2 usunięcie danych

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
-import {FacesComponentModule} from "./ui/faces/faces.component-module";
-import {EmployeeServiceModule} from "./services/employee.service-module";
-import {ProjectListComponentModule} from "./ui/project-list-component/project-list.component-module";
-import {ProjectServiceModule} from "./services/project.service-module";
-import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
-import {EmployeeFormComponent} from "./ui/employee-form/employee-form.component";
 
 
 @NgModule({
@@ -18,12 +11,6 @@ import {EmployeeFormComponent} from "./ui/employee-form/employee-form.component"
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    EmployeeListComponentModule,
-    EmployeeServiceModule,
-    FacesComponentModule,
-    ProjectListComponentModule,
-    ProjectServiceModule,
-    EmployeeFormComponentModule,
 
   ],
   providers: [],
--- src/app/services/employee.response.ts
+++
@@ -1,5 +0,0 @@
-export interface EmployeeResponse {
-  readonly id: string;
-  readonly employee_name: string;
-  readonly profile_image: string;
-}
